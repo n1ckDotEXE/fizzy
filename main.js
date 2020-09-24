f95dbe9377da397dea448853136f86bbf7061963
n1ckDotEXE <admin@nicholasnam.com>
2020-09-24 12:05:21 -0400

    Completed

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function fizzy(num) {
-
+  if (num % 3 === 0 && num % 5 !== 0) {
+    return "Fizz";
+  } else if (num % 5 === 0 && num % 3 !== 0) {
+    return "Buzz";
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else { // (num % 3 !== 0 && num % 5 !== 0)
+    return num;
+  }
 }
 
 module.exports = {
